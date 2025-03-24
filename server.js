1e236028fafa9a16224a5859d137e5cd1ad53f55
cykoravish <ravishbisht85@gmail.com>
2025-03-24 12:26:46 +0530

    cors error debugging

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["https://skywings-frontend.onrender.com", "http://localhost:5173"], // Only allow frontend
+    origin: ["https://skywings-frontend.onrender.com", "http://localhost:5173", process.env.FORNTEND_URL], // Only allow frontend
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
     credentials: true, // If using cookies/auth tokens
   })
